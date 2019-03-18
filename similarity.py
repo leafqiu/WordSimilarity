1065fcd3e2f3044104c4e0d3c1470d073b9c0d68
leafqiu <leafqiu@foxmail.com>
2019-03-18 12:16:14 +0800

    Path-based similarity.

--- similarity.py
+++ similarity.py
@@ -3,11 +3,24 @@ import pandas as pd
 import numpy as np
 
 
+def word_similarity_wordnet(word_pairs):
+    path_scores = []
+    for (first_word, second_word) in word_pairs:
+        first_synset = wn.synsets(first_word)
+        second_synset = wn.synsets(second_word)
+        maxsim = 0
+        for word1 in first_synset:
+            for word2 in second_synset:
+                lch_sim = word1.path_similarity(word2)
+                if lch_sim is not None:
+                    maxsim = max(maxsim, lch_sim)
+        path_scores.append(maxsim)
+    # print(path_scores[:10])
+    return path_scores
 
-def word_similarity_wordnet(word_pairs, sim_scores):
-    for pair in word_pairs:
-        # TODO: computing similarity between two words
-        pass
+
+def compare_similarity(scores, labels):
+    pass
 
 
 if __name__ == '__main__':
@@ -18,4 +31,5 @@ if __name__ == '__main__':
     for pair in data.get_values():
         word_pairs.append(tuple(pair[:2]))
         sim_scores.append(pair[2])
-    word_similarity_wordnet(word_pairs, sim_scores)
+    path_scores = word_similarity_wordnet(word_pairs, sim_scores)
+    compare_similarity(path_scores, sim_scores)
